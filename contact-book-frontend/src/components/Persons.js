475fe7fddfc72ea2e22aae66033260ab8059fec9
Kyaw Thet Aung <billionairekta@gmail.com>
2021-11-14 23:21:43 +0630

    refactored with dev server

--- contact-book-frontend/src/components/Persons.js
+++ contact-book-frontend/src/components/Persons.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Persons = ({ nameToShow }) => {
-    const showNames = nameToShow.map( person => <div key={person.name} >{person.name} {person.number}<br/> </div>)
+    const showNames = nameToShow.map( person => <div key={person.name} >{person.name} {person.number}<br/> </div> )
     return (
         <>
             {showNames}
